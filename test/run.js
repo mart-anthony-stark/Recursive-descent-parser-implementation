e677299957af21ffb2b650afeb4509f932a11558
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-25 20:26:43 +0800

    fixed: json format

--- test/run.js
+++ test/run.js
@@ -3,4 +3,4 @@ const { Parser } = require("../src/Parser");
 const parser = new Parser();
 
 const ast = parser.parse("2155");
-console.log(JSON.stringify(ast));
+console.log(JSON.stringify(ast, null, 2));
