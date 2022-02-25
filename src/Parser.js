4716648ca1ee6766af6afcf85b5c809f3eb7cb3c
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-25 20:58:55 +0800

    add: predictive parsing

--- src/Parser.js
+++ src/Parser.js
@@ -1,12 +1,28 @@
 /**
  * Palabra parser implemented in recursive descent.
  */
+const { Tokenizer } = require("./Tokenizer");
+
 class Parser {
+  /**
+   * Initializes the parser.
+   */
+  constructor() {
+    this._string = "";
+    this._tokenizer = new Tokenizer();
+  }
+
   /**
    * Parses a string into abstract syntax tree
    */
   parse(string) {
     this._string = string;
+    this._tokenizer.init(string);
+
+    // Prime the tokenizer to obtain first
+    // token which is our lookahead. The lookahead is
+    // used for predictive parsing
+    this.lookahead = this._tokenizer.getNextToken();
 
     // Parse recursively starting from the main
     // entry point, the Program:
