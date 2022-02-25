fc068d6d8f82480abfe54c51cada8917d8a0a6d0
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-25 21:57:01 +0800

    fixed: test case

--- test/run.js
+++ test/run.js
@@ -2,5 +2,5 @@ const { Parser } = require("../src/Parser");
 
 const parser = new Parser();
 
-const ast = parser.parse("212");
+const ast = parser.parse("45a");
 console.log(JSON.stringify(ast, null, 2));
