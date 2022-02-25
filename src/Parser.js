580eb197a89f21525cbacf87a97478dc5328c3d1
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-25 19:58:10 +0800

    add: parser class

--- src/Parser.js
+++ src/Parser.js
@@ -1,3 +1,15 @@
 /**
  * Palabra parser implemented in recursive descent.
  */
+class Parser {
+  /**
+   * Parses a string into abstract syntax tree
+   */
+  parse(string){
+
+  }
+}
+
+module.exports = {
+    Parser
+}
