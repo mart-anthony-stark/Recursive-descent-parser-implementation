7f34679511ac0d9197dacec4cb9bcadc58dfea49
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-25 20:02:05 +0800

    add: numeric literal

--- src/Parser.js
+++ src/Parser.js
@@ -5,11 +5,32 @@ class Parser {
   /**
    * Parses a string into abstract syntax tree
    */
-  parse(string){
-
+  parse(string) {
+    this._string = string;
+  }
+  /**
+   * Main entry point
+   *
+   * Program
+   *    : NumericLiteral
+   *    ;
+   */
+  Program() {
+    return this.NumericLiteral();
+  }
+  /**
+   * NumericLiteral
+   *    : NUMBER
+   *    ;
+   */
+  NumericLiteral() {
+    return {
+      type: "NumericLiteral",
+      value: Number(this._string),
+    };
   }
 }
 
 module.exports = {
-    Parser
-}
+  Parser,
+};
