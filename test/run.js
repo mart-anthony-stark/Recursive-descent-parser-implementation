a77d3b3a0eb55c8753862407f947c5f0623c7001
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-25 21:51:29 +0800

    mod

--- test/run.js
+++ test/run.js
@@ -2,5 +2,5 @@ const { Parser } = require("../src/Parser");
 
 const parser = new Parser();
 
-const ast = parser.parse("2155");
+const ast = parser.parse("212");
 console.log(JSON.stringify(ast, null, 2));
