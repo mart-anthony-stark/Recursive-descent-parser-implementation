13ac4d88f5ab07e7ebdc604997a4ece42a63f20b
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-26 20:48:27 +0800

    add: syntax error

--- test/run.js
+++ test/run.js
@@ -2,6 +2,6 @@ const { Parser } = require("../src/Parser");
 
 const parser = new Parser();
 
-const program = `'26'`;
+const program = `654`;
 const ast = parser.parse(program);
 console.log(JSON.stringify(ast, null, 2));
