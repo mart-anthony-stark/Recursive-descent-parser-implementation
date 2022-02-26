df863e25537d82b1e16c1711891598efba717709
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-26 20:55:01 +0800

    add: whitespaces

--- test/run.js
+++ test/run.js
@@ -2,6 +2,6 @@ const { Parser } = require("../src/Parser");
 
 const parser = new Parser();
 
-const program = `654`;
+const program = `  45`;
 const ast = parser.parse(program);
 console.log(JSON.stringify(ast, null, 2));
