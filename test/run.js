1fbb042312bd65f49253c5920e5333d64880992d
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-26 20:44:07 +0800

    optimized tokenizer

--- test/run.js
+++ test/run.js
@@ -2,6 +2,6 @@ const { Parser } = require("../src/Parser");
 
 const parser = new Parser();
 
-const program = `"615"`;
+const program = `'26'`;
 const ast = parser.parse(program);
 console.log(JSON.stringify(ast, null, 2));
